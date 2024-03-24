e23f8b7344e29c9ae109175b215493731789a529
mohamedesmat1997 <158211063+Esmat-97@users.noreply.github.com>
2024-03-24 22:11:38 +0200

    hero section

--- app/main.php
+++ app/main.php
@@ -1,4 +1,28 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="../css/hero.css">
+</head>
+<body>
+    
+
 <?php include "header.php" ?>
 
+<br>
+
+<header class="hero-section">
+    <div class="content">
+     
+        <p class="sub-heading"> <strong> best products collection of all time</strong></p>
+    </div>
+</header>
+
+<br>
 
 <?php include "footer.php" ?>
+
+</body>
+</html>
