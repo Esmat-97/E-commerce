bb77a7fa550d68f882c7bf6a2785509cf3fee08c
mohamedesmat1997 <158211063+Esmat-97@users.noreply.github.com>
2024-04-16 02:36:54 +0200

    show more product

--- app/products.php
+++ app/products.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
 </head>
@@ -49,6 +50,12 @@ $comer=$_COOKIE['userrole'];
         <input type="hidden" name="protodel" value="<?php echo $row['product_name']  ?>">
         <input type="submit" value="delete" name="delpro">
        </form>
+
+       <form action="../operations/showpro.php" method="post">
+        <input type="hidden" name="protoshow" value="<?php echo $row['product_name']  ?>">
+        <input type="submit" value="show more" name="showpro">
+       </form>
+
        <?php
    }
    ?>
@@ -56,6 +63,7 @@ $comer=$_COOKIE['userrole'];
     </div>
   </div>
 
+  <?php include '../operations/showpro.php' ?>
 
 
 <?php
@@ -77,7 +85,7 @@ $conn = null;
 ?>
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 
 <?php include '../operations/delproducts.php' ?>
 
